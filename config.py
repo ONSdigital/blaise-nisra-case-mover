03036b65f6bc1fe0a695271cf297959a9962caaf
elthorne <46898565+elthorne@users.noreply.github.com>
2020-12-01 16:20:36 +0000

    Refactored config to set default blank string to instrument_source_path if None

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from os import environ
 
 load_dotenv()
 
-instrument_source_path = os.getenv('INSTRUMENT_SOURCE_PATH')
+instrument_source_path = os.getenv('INSTRUMENT_SOURCE_PATH', '')
 survey_source_path = os.getenv('SURVEY_SOURCE_PATH')
 instrument_destination_path = os.getenv('INSTRUMENT_DESTINATION_PATH')
 bucket_name = os.getenv('NISRA_BUCKET_NAME')
