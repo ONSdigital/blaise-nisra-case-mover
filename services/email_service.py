d7df6a99f8ced6355d5a18907f2bcb403049f7e7
jamie kerr <jamiestephenkerr@gmail.com>
2022-11-11 13:19:30 +0000

    makde datetime timzone aware

--- services/email_service.py
+++ services/email_service.py
@@ -1,19 +1,22 @@
 from notifications_python_client import NotificationsAPIClient
 
+from models.configuration.email_config_model import EmailConfig
+
 
 class EmailService:
-    def __init__(self, email_client: NotificationsAPIClient):
-        self._email_client = email_client
+    def __init__(self, config: EmailConfig):
+        self._email_client = NotificationsAPIClient(config.notify_api_key)
+        self._email_address = config.to_notify_email
+        self._email_template_id = "94264180-7ebd-4ff9-8a27-52abb5949c78"
 
-    def send_email_notification(self, email_address: str, email_template_id: str, questionnaire_name: str) -> None:
+    def send_email_notification(self, questionnaire_name: str) -> None:
         try:
-            print(f"Sending email notification for questionnaire {questionnaire_name} to {email_address}")
+            print(f"Sending email notification for questionnaire {questionnaire_name} to {self._email_address}")
             self._email_client.send_email_notification(
-                email_address=f"{email_address}",
-                template_id=f"{email_template_id}",
+                email_address=f"{self._email_address}",
+                template_id=f"{self._email_template_id}",
                 personalisation={"questionnaire_name": questionnaire_name}
             )
         except Exception as error:
             print(
-                f"Error when sending email notification for questionnaire {questionnaire_name} via GOV.UK Notify API - ",
-                error)
+                f"Error when sending email for questionnaire {questionnaire_name} via GOV.UK Notify API - ", error)
