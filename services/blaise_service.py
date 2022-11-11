08dd496e7e3d330676e97f0423f7eacb7ebd23d7
jamie kerr <jamiestephenkerr@gmail.com>
2022-11-11 07:42:23 +0000

    ugh need to sort out this config hell

--- services/blaise_service.py
+++ services/blaise_service.py
@@ -8,7 +8,7 @@ from models.configuration.blaise_config_model import BlaiseConfig
 class BlaiseService:
     def __init__(self, config: BlaiseConfig):
         self._config = config
-        self.restapi_client = blaise_restapi.Client(self._config.blaise_api_url)
+        self.restapi_client = blaise_restapi.Client(f"http://{self._config.blaise_api_url}")
 
     def get_instruments(self) -> List[Dict[str, Any]]:
         return self.restapi_client.get_all_questionnaires_for_server_park(self._config.server_park)
