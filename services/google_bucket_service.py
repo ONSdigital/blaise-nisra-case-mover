e1032f0ff435bf8ea9fe1c7647e6b6bf435e441a
jamie kerr <jamiestephenkerr@gmail.com>
2022-11-14 12:55:49 +0000

    added tests for buvket service

--- services/google_bucket_service.py
+++ services/google_bucket_service.py
@@ -1,6 +1,6 @@
 import logging
 from datetime import datetime
-from typing import Dict
+from typing import Dict, List
 
 from google.cloud import storage
 
@@ -9,19 +9,23 @@ from models.configuration.bucket_config_model import BucketConfig
 
 class GoogleBucketService:
     def __init__(self, config: BucketConfig):
-        self._bucket = storage.Client().get_bucket(config.bucket_name)
+        self._bucket_name = config.bucket_name
 
     def get_questionnaire_modified_dates(self, file_extension: str) -> Dict[str, datetime]:
         try:
             questionnaire_modified_dates = {}
-            blobs = list(self._bucket.list_blobs())
+            blobs = self.get_blobs()
 
             for blob in blobs:
-                if blob.name.upper().endswith(file_extension.upper()):
+                if not blob.name.upper().endswith(file_extension.upper()):
                     continue
 
                 questionnaire_name = blob.name.upper().split("/")[0]
                 questionnaire_modified_dates[questionnaire_name] = blob.updated
             return questionnaire_modified_dates
         except Exception as error:
-            logging.error("GoogleStorageService: error in calling 'get_files_from_bucket'", error)
+            logging.error(f"GoogleStorageService: error in calling 'get_files_from_bucket' - {error}")
+
+    def get_blobs(self) -> List:
+        _storage_client = storage.Client()
+        return list(_storage_client.list_blobs(self._bucket_name))
