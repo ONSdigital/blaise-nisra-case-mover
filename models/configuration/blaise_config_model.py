73db06e9ab782a7559476202de378c7a2e4c7804
jamie kerr <jamiestephenkerr@gmail.com>
2022-11-14 13:54:32 +0000

    trying to work out why the other config tests are failing

--- models/configuration/blaise_config_model.py
+++ models/configuration/blaise_config_model.py
@@ -9,12 +9,12 @@ class BlaiseConfig:
     server_park: str
 
     @classmethod
-    def from_env(cls) -> "BlaiseConfig":
+    def from_env(cls):
         return cls(
             blaise_api_url=os.getenv("BLAISE_API_URL", ""),
             server_park=os.getenv("SERVER_PARK", "")
         )
 
-    def log(self) -> None:
+    def log(self):
         logging.info(f"blaise_api_url: {self.blaise_api_url}")
         logging.info(f"server_park: {self.server_park}")
