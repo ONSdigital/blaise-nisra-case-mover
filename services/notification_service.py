45a256697c55b190e92ef4f91a5c2aefb4b84ecc
jamie kerr <jamiestephenkerr@gmail.com>
2022-11-15 14:21:08 +0000

    refactored notification service

--- services/notification_service.py
+++ services/notification_service.py
@@ -9,20 +9,19 @@ class NotificationService:
     def __init__(self, config: NotificationConfig):
         self._email_client = NotificationsAPIClient(config.notify_api_key)  # type: ignore
         self._email_address = config.nisra_notify_email
-        self._email_template_id = config.email_template_id
 
-    def send_email_notification(self, questionnaire_name: str) -> None:
+    def send_email_notification(self, message: dict[str, str], template_id: str) -> None:
         try:
             logging.info(
-                f"Sending email notification for questionnaire {questionnaire_name} to {self._email_address}"
+                f"Sending email notification {message} to {self._email_address}"
             )
             self._email_client.send_email_notification(  # type: ignore
                 email_address=f"{self._email_address}",
-                template_id=f"{self._email_template_id}",
-                personalisation={"questionnaire_name": questionnaire_name},
+                template_id=f"{template_id}",
+                personalisation=message,
             )
         except Exception as error:
             logging.error(
-                f"NotificationService: Error when sending email for questionnaire {questionnaire_name} "
-                f"via GOV.UK Notify API - {error}"
+                f"NotificationService: Error when sending email via GOV.UK Notify API - {error}"
             )
+            raise error
