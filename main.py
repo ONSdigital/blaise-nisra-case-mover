9cdea6e2058789b737cabd326d9e40d8c08450d0
James Anthony Williams <81305008+JAWilliamsONS@users.noreply.github.com>
2022-11-22 12:10:16 +0000

    feat: add try, except to cloud functions Give more specific errors refs: BLAIS5-3471

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ from services.google_bucket_service import GoogleBucketService
 from services.nisra_update_check_service import NisraUpdateCheckService
 from services.notification_service import NotificationService
 from util.service_logging import setupLogging
-
+from exceptions import TriggerError, ProcessorError
 
 def ssh_retry_logger():
     logging.info("Retrying for SSH Exception")
@@ -43,50 +43,53 @@ def trigger(*args, **kwargs):
 
 
 def do_trigger(event, _context):
-    trigger_event = TriggerEvent.from_json(
-        base64.b64decode(event["data"]).decode("utf-8")
-    )
-    print(f"Nisra triggered for survey: '{trigger_event.survey}'")
-    config = Config.from_env()
-    sftp_config = SFTPConfig.from_env()
-    config.log()
-    sftp_config.survey_source_path = trigger_event.survey
-    sftp_config.log()
-    publisher_client = pubsub_v1.PublisherClient()
+    try:
+        trigger_event = TriggerEvent.from_json(
+            base64.b64decode(event["data"]).decode("utf-8")
+        )
+        print(f"Nisra triggered for survey: '{trigger_event.survey}'")
+        config = Config.from_env()
+        sftp_config = SFTPConfig.from_env()
+        config.log()
+        sftp_config.survey_source_path = trigger_event.survey
+        sftp_config.log()
+        publisher_client = pubsub_v1.PublisherClient()
 
-    cnopts = pysftp.CnOpts()
-    cnopts.hostkeys = None
-    cnopts.compression = True
+        cnopts = pysftp.CnOpts()
+        cnopts.hostkeys = None
+        cnopts.compression = True
 
-    google_storage = init_google_storage(config)
-    if google_storage.bucket is None:
-        return "Connection to bucket failed", 500
+        google_storage = init_google_storage(config)
+        if google_storage.bucket is None:
+            return "Connection to bucket failed", 500
 
-    logging.info("Connecting to SFTP server")
-    with pysftp.Connection(
-        host=sftp_config.host,
-        username=sftp_config.username,
-        password=sftp_config.password,
-        port=int(sftp_config.port),
-        cnopts=cnopts,
-    ) as sftp_connection:
-        logging.info("Connected to SFTP server")
+        logging.info("Connecting to SFTP server")
+        with pysftp.Connection(
+            host=sftp_config.host,
+            username=sftp_config.username,
+            password=sftp_config.password,
+            port=int(sftp_config.port),
+            cnopts=cnopts,
+        ) as sftp_connection:
+            logging.info("Connected to SFTP server")
 
-        sftp = SFTP(sftp_connection, sftp_config, config)
-        case_mover = CaseMover(google_storage, config, sftp)
-        instruments = get_filtered_instruments(sftp, case_mover)
-        logging.info(f"Processing survey - {sftp_config.survey_source_path}")
+            sftp = SFTP(sftp_connection, sftp_config, config)
+            case_mover = CaseMover(google_storage, config, sftp)
+            instruments = get_filtered_instruments(sftp, case_mover)
+            logging.info(f"Processing survey - {sftp_config.survey_source_path}")
 
-        if len(instruments) == 0:
-            logging.info("No instrument folders found after filtering")
-            return "No instrument folders found, exiting", 200
+            if len(instruments) == 0:
+                logging.info("No instrument folders found after filtering")
+                return "No instrument folders found, exiting", 200
 
-        for instrument_name, instrument in instruments.items():
-            trigger_processor(
-                publisher_client,
-                config,
-                ProcessorEvent(instrument_name=instrument_name, instrument=instrument),
-            )
+            for instrument_name, instrument in instruments.items():
+                trigger_processor(
+                    publisher_client,
+                    config,
+                    ProcessorEvent(instrument_name=instrument_name, instrument=instrument),
+                )
+    except TriggerError:
+        exit(1)
 
 
 def processor(*args, **kwargs):
@@ -103,40 +106,43 @@ def processor(*args, **kwargs):
 
 
 def do_processor(event, _context):
-    config = Config.from_env()
-    sftp_config = SFTPConfig.from_env()
-    config.log()
-    sftp_config.log()
+    try:
+        config = Config.from_env()
+        sftp_config = SFTPConfig.from_env()
+        config.log()
+        sftp_config.log()
 
-    cnopts = pysftp.CnOpts()
-    cnopts.hostkeys = None
-    cnopts.compression = True
+        cnopts = pysftp.CnOpts()
+        cnopts.hostkeys = None
+        cnopts.compression = True
 
-    google_storage = init_google_storage(config)
-    if google_storage.bucket is None:
-        return "Connection to bucket failed", 500
+        google_storage = init_google_storage(config)
+        if google_storage.bucket is None:
+            return "Connection to bucket failed", 500
 
-    logging.info("Connecting to SFTP server")
+        logging.info("Connecting to SFTP server")
 
-    with pysftp.Connection(
-        host=sftp_config.host,
-        username=sftp_config.username,
-        password=sftp_config.password,
-        port=int(sftp_config.port),
-        cnopts=cnopts,
-    ) as sftp_connection:
-        logging.info("Connected to SFTP server")
+        with pysftp.Connection(
+            host=sftp_config.host,
+            username=sftp_config.username,
+            password=sftp_config.password,
+            port=int(sftp_config.port),
+            cnopts=cnopts,
+        ) as sftp_connection:
+            logging.info("Connected to SFTP server")
 
-        sftp = SFTP(sftp_connection, sftp_config, config)
-        case_mover = CaseMover(google_storage, config, sftp)
+            sftp = SFTP(sftp_connection, sftp_config, config)
+            case_mover = CaseMover(google_storage, config, sftp)
 
-        processor_event = ProcessorEvent.from_json(
-            base64.b64decode(event["data"]).decode("utf-8")
-        )
+            processor_event = ProcessorEvent.from_json(
+                base64.b64decode(event["data"]).decode("utf-8")
+            )
 
-        process_instrument(
-            case_mover, processor_event.instrument_name, processor_event.instrument
-        )
+            process_instrument(
+                case_mover, processor_event.instrument_name, processor_event.instrument
+            )
+    except ProcessorError:
+        exit(1)
 
 
 def nisra_changes_checker(_event, _context):
