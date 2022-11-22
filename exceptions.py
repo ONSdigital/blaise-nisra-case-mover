253fc3b35ad81b3ab45788dc19e6a3e3767f8dff
James Anthony Williams <81305008+JAWilliamsONS@users.noreply.github.com>
2022-11-22 14:04:30 +0000

    chore: fix typo Thanks El refs: BLAIS5-3471

--- exceptions.py
+++ exceptions.py
@@ -7,9 +7,9 @@ class Error(Exception):
 
 class TriggerError(Error):
     """Raised when the trigger cloud function fails"""
-    logging.error(f"Trigger failed due to a an Exception. {Error}")
+    logging.error(f"Trigger failed due to an Exception. {Error}")
 
 
 class ProcessorError(Error):
     """Raised when the processor cloud function fails"""
-    logging.error(f"Processor failed due to a an Exception. {Error}")
+    logging.error(f"Processor failed due to an Exception. {Error}")
