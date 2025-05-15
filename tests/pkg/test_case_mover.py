0211a9e54b807d5c94d514d46ab13b968da2ac99
SidraJaved <16195224+SidraJaved@users.noreply.github.com>
2025-05-15 11:53:24 +0100

    increasing connectTimeout to 2, while keeping readTimeout as 1, and also added 1 retry if fails. with some additional logs

--- tests/pkg/test_case_mover.py
+++ tests/pkg/test_case_mover.py
@@ -298,7 +298,7 @@ def test_send_request_to_api(mock_requests_post, case_mover, config):
         ),
         json={"questionnaireDataPath": "opn2101a"},
         headers={"content-type": "application/json"},
-        timeout=1,
+        timeout=(2,1),
     )
 
 
