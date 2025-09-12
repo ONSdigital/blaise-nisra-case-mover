e9c4b458e628b2e40703398e6dac30afa0209d54
SidraJaved <16195224+SidraJaved@users.noreply.github.com>
2025-09-12 12:00:35 +0100

    wip

--- tests/features/environment.py
+++ tests/features/environment.py
@@ -11,7 +11,6 @@ from pkg.google_storage import GoogleStorage
 from pkg.sftp import SFTPConfig
 from util.service_logging import setupLogging
 
-
 class FakePublisherClient(PublisherClient):
     def __init__(self):
         self.published_messages = []
@@ -24,42 +23,47 @@ class FakePublisherClient(PublisherClient):
         for message in self.published_messages:
             event = {"data": base64.encodebytes(message["data"])}
             main.processor(event, {})
+    
+def before_all(context):
+    context.config = Config.from_env()
+    context.sftp_config = SFTPConfig.from_env()
 
 
-    def before_all(context):
-        context.config = Config.from_env()
-        context.sftp_config = SFTPConfig.from_env()
+def before_feature(context, feature):
+    print(f"Feature: {feature.name}")
+    setupLogging()
+    context.publisher_client = FakePublisherClient()
 
+def before_scenario(context, scenario):
+    print(f"Scenario: {scenario.name}")
+    context.mock_requests_post = None
+    context.config = Config.from_env()
+    context.sftp_config = SFTPConfig.from_env()
+    
 
-    def before_feature(context, feature):
-        setupLogging()
-        context.publisher_client = FakePublisherClient()
+def after_scenario(context, scenario):
+    try:
+        print(f"Published messages: {context.publisher_client.published_messages}")
+        context.publisher_client.published_messages = []  
 
+        google_storage = GoogleStorage(os.getenv("NISRA_BUCKET_NAME", "env_var_not_set"))
+        google_storage.initialise_bucket_connection()
+        if google_storage.bucket is None:
+            print("Failed")
 
-    def after_scenario(context, scenario):
+        blobs = google_storage.list_blobs()
 
-        try:
-            print(f"Published messages: {context.publisher_client.published_messages}")
-            context.publisher_client.published_messages = []  
-
-            google_storage = GoogleStorage(os.getenv("NISRA_BUCKET_NAME", "env_var_not_set"))
-            google_storage.initialise_bucket_connection()
-            if google_storage.bucket is None:
-                print("Failed")
-
-            blobs = google_storage.list_blobs()
-
-            google_storage.delete_blobs(blobs)
-            cnopts = pysftp.CnOpts()
-            cnopts.hostkeys = None
-            with pysftp.Connection(
-                host=context.sftp_config.host,
-                username=context.sftp_config.username,
-                password=context.sftp_config.password,
-                port=int(context.sftp_config.port),
-                cnopts=cnopts,
-            ) as sftp:
-                sftp.execute("rm -rf ~/ONS/TEST/OPN2101A")
-        except Exception as e:
-            print(f"Ignored SFTP cleanup error: {e}", file=sys.__stderr__, flush=True)
+        google_storage.delete_blobs(blobs)
+        cnopts = pysftp.CnOpts()
+        cnopts.hostkeys = None
+        with pysftp.Connection(
+            host=context.sftp_config.host,
+            username=context.sftp_config.username,
+            password=context.sftp_config.password,
+            port=int(context.sftp_config.port),
+            cnopts=cnopts,
+        ) as sftp:
+            sftp.execute("rm -rf ~/ONS/TEST/OPN2101A")
+    except Exception as e:
+        print(f"Ignored SFTP cleanup error: {e}", file=sys.__stderr__, flush=True)
         
