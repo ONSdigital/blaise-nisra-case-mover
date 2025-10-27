05719eee2770efdd106e49314e3e460dda1ffd88
SidraJaved <16195224+SidraJaved@users.noreply.github.com>
2025-10-27 10:30:51 +0000

    wip: usign sftp host to identify if its test/ci env or production to add AutoAdd or Reject policy respectively.
    refs: BLAIS%-4953

--- util/sftp_connection.py
+++ util/sftp_connection.py
@@ -1,13 +1,22 @@
 from contextlib import contextmanager
+import logging
 
 import paramiko
 
 
 @contextmanager
-def sftp_connection(sftp_config):
+def sftp_connection(sftp_config, allow_unknown_hosts: bool = False):
 
     ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    if allow_unknown_hosts or sftp_config.host in ("localhost", "127.0.0.1"):
+        logging.warning("Accepting unknown host keys! Only use this in test environments.")
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    else:
+        ssh.load_system_host_keys()
+        ssh.set_missing_host_key_policy(paramiko.RejectPolicy())
+        
+    #ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(
         hostname=sftp_config.host,
         username=sftp_config.username,
