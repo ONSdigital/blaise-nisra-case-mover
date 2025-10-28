05eebb4adc176d141eea85239be66abe288d7f1f
SidraJaved <16195224+SidraJaved@users.noreply.github.com>
2025-10-28 11:08:52 +0000

    wip

--- util/sftp_connection.py
+++ util/sftp_connection.py
@@ -30,7 +30,9 @@ def sftp_connection(
             f"⚠️ Accepting unknown host keys for {host}. "
             "Only safe for dev/test environments."
         )
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy()) # codeql: ignore [AcceptingUnknownHostKey] - Required for current prod setup
+        ssh.set_missing_host_key_policy(
+            paramiko.AutoAddPolicy()
+        )  # codeql: ignore [AcceptingUnknownHostKey] - Required for current prod setup
     else:
         # Production: reject unknown hosts
         ssh.load_system_host_keys()
